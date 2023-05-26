61edce4cd7bd73db66a96675d3374e5d03b557b4
Aniket <aniketmallick0007@gmail.com>
2023-05-26 11:30:11 +0530

    corrected

--- src/pages/searchResults/SearchResults.jsx
+++ src/pages/searchResults/SearchResults.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 import "./style.scss";
 
-import { fetchDataFromApi } from "../../utils/api";
+import { fetchDataFromApi } from "../../utils/Api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import MovieCard from "../../components/movieCard/MovieCard";
 import Spinner from "../../components/spinner/Spinner";
-import noResults from "../../assets/no-results.png";
+
 
 const SearchResults = () => {
   const [data, setData] = useState(null);
